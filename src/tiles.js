d62a000c905f18febbcb94786b1a4b20f22eb2e4
Marco Bolis <mbolis1984@gmail.com>
2015-02-09 19:01:52 +0100

    Rework everything into a more manageable and simple mass

--- src/tiles.js
+++ src/tiles.js
@@ -1,327 +1,278 @@
-Crafty.c('_GridRoot', {
+function Palette(data) {
+	this.name = (data.name || 'palette_' + (++Palette.uuid)).trim().replace(/[\s,]/g, '_');
+	this._terrain = {};
+	if (typeof data.terrain === 'object') {
+		var terrain = data.terrain;
+		for (var code in terrain) {
+			if (terrain.hasOwnProperty(code)) {
+				if (this._terrain[code])
+					console.log('WARNING [' + this.name +'] duplicate terrain code "' + code + '"!');
+				var component = this._terrain[code] = this.name + '#' + (++Palette.tuid);
+				var spec = terrain[code];
+				if (spec.color) {
+					this.color(component, spec);
+				} else if (t.sprite) {
+					this.sprite(component, spec);
+				}
+			}
+		}
+	}
+	if (typeof data.features === 'object') {
+		this._features = features;
+	}
+}
+Palette.uuid = 0;
+Palette.tuid = 0;
+Palette.fuid = 0;
+Palette.prototype.color = function(code, spec) {
+	return Crafty.c(code, {
+		init : function() {
+			this.requires('Grid, Canvas, Color').color(spec.color);
+			if (spec.size) this.size(spec.size);
+		},
+		_mapspec : spec
+	});
+};
+Palette.prototype.sprite = function(code, spec) {
+	throw new Error('Unsupported operation.');
+	return Crafty.c(this.name + '#' + code, {
+		init : function() {
+			this.requires('Grid, Canvas, Color').color(spec.color);
+			if (spec.size) this.size(spec.size);
+		},
+		_mapspec : spec
+	});
+	
+};
+Palette.prototype.terrain = function(code) {
+	return Crafty.e(this._terrain[code]);
+}
+
+function Map(opt) {
+	P(this)
+		('hidden sealed').def('_terrain')
+		('hidden sealed').def('_features')
+		('hidden sealed').def('_palette')
+		('hidden sealed').def('_rows', 0)
+		('hidden sealed').def('_cols', 0)
+		('hidden sealed').def('_showing')
+//		('hidden sealed').constant('_astar', new EasyStar.js);
+
+	if (typeof opt === 'object') {
+		if (typeof opt.rows === 'number' && typeof opt.cols === 'number') {
+			this.size(opt.rows, opt.cols);
+		}
+		if (opt.terrain instanceof Array) {
+			this.terrain(opt.terrain);
+		}
+		if (opt.features instanceof Array) {
+			this.features(opt.features);
+		}
+		if (opt.palette instanceof Palette) {
+			this.palette(opt.palette);
+		}
+	}
+}
+Map.unit = { w : 32, h : 32 };
+Map.prototype.size = function(rows, cols) {
+	if (typeof rows === 'undefined' && typeof cols === 'undefined') {
+		return { rows : this._rows, cols : this._cols };
+	}
+	this._rows = rows || this._rows;
+	this._cols = cols || this._cols;
+	return this;
+}
+Map.prototype.terrain = function(tiles) {
+	if (!tiles) return this._terrain;
+	this._terrain = [];
+	var cols = 0;
+	for (var r = 0; r < tiles.length; r++) {
+		var row = [];
+		for (var c = 0; c < tiles[r].length; c++) {
+			row[c] = tiles[r][c];
+			cols = Math.max(cols, tiles[r].length);
+		}
+		this._terrain.push(row);
+	}
+	if (!this._rows) this._rows = tiles.length;
+	if (!this._cols) this._cols = cols;
+	return this;
+}
+Map.prototype.terrainAt = function(row, col, tile) {
+	if (!tile) return this._grid._terrain[row + ':' + col];
+	this._terrain[row][col] = tile;
+	if (this._showing) {
+	var t = this._grid._terrain[row + ':' + col];
+	if (t) t.destroy();
+		this._grid._terrain[row + ':' + col] = this._palette.terrain(tile);
+	}
+},
+Map.prototype.features = function(tiles) {
+	if (!tiles) return this._features;
+	this._features = [];
+	var cols = 0;
+	for (var r = 0; r < tiles.length; r++) {
+		var row = [];
+		for (var c = 0; c < tiles[r].length; c++) {
+			row[c] = tiles[r][c];
+			cols = Math.max(cols, tiles[r].length);
+		}
+		this._features.push(row);
+	}
+	if (!this._rows) this._rows = tiles.length;
+	if (!this._cols) this._cols = cols;
+	return this;
+}
+Map.prototype.palette = function(palette) {
+	if (!palette) return this._palette;
+	this._palette = palette;
+	return this;
+}
+Map.prototype.show = function() {
+	if (this._showing) return;
+
+	var terrain = this._terrain;
+	var features = this._features;
+	if (!terrain && !features) throw new Error('Map terrain or features must be defined');
+	var palette = this._palette;
+	if (!palette) throw new Error('Map palette must be defined');
+
+	var grid = this._grid = Crafty.e('GridSpace');
+	P(grid)
+		('hidden sealed').constant('_terrain', {})
+		('hidden sealed').constant('_features', {});
+	for (var r = 0; r < this._rows; r++) {
+		for (var c = 0; c < this._cols; c++) {
+			var key, entity, coords = r + ':' + c;
+			if (terrain) {
+				key = terrain[r][c];
+				entity = palette.terrain(key).coords(r, c);
+				if (entity) {
+					grid.attach(entity);
+					grid._terrain[coords] = entity;
+				}
+			}
+			if (features) {
+				key = features[r][c];
+				entity = palette.features(key).coords(r, c);
+				if (entity) {
+					grid.attach(entity);
+					grid._features[coords] = entity;
+				}
+			}
+		}
+	}
+
+	this._showing = true;
+
+	var width = this._rows * Map.unit.h, height = this._cols * Map.unit.w;
+	Crafty.viewport.bounds = {
+		min : { x : 0, y : 0 },
+		max : { x : width, y : height }
+	};
+	grid.bind('Move', function() {
+		Crafty.viewport.bounds = {
+			min : { x : grid._x, y : grid._y },
+			max : { x : grid._x + width, y : grid._y + height }
+		}
+	});
+}
+Map.prototype.hide = function() {
+	if (!this._showing) return;
+	this._grid.destroy();
+	this._showing = false;
+	Crafty.viewport.bounds = null;
+}
+
+Crafty.c('GridSpace', {
 	init : function() {
 		this.requires('2D');
-	}
-})
-
-Grid = {
-	root : undefined,
-	count : 0,
-}
+	},
+});
 
 Crafty.c('Grid', {
-	_unit : { _w : 32, _h : 32 },
 	init : function() {
-		if (!Grid.root) {
-			Grid.root = Crafty.e('_GridRoot');
-		}
-		Grid.root.attach(this);
+		var self = this.requires('2D')
+			.coords(0, 0)
+			.size(1, 1);
 
-		var grid = this.requires('2D').attr({
-			x : Grid.root._x,
-			y : Grid.root._y
-		});
-	
-		this._gridId = '_grid' + (++Grid.count);
-		this._tileCID = this._gridId + '::Tile';
-		Crafty.c(this._tileCID, {
-			init : function() {
-				this.requires('Tile').place(grid);
-			}
-		});
-
-		var atc = this.attach;
-		this.attach = function(c) {
-			atc.call(this, c);
-			if (c && c.has('Tile')) {
-				c.w = this._unit._w;
-				c.h = this._unit._h;
-			}
-			return this;
-		}
-
-		var dtc = this.detach;
-		this.detach = function(c) {
-			dtc.call(this, c);
-			if (c && c.has('Tile')) {
-			}
-			return this;
-		}
-
-		Object.defineProperties(this, {
-			_gridId : {
-				writable : false,
-				enumerable : false
-			},
-			_unit : { 
-				writable : true,
-				enumerable : false
-			},
-			unit : {
-				enumerable : true,
-				configurable : true,
-				get : function() { return this._unit },
-				set : function(u) {
-					var w = u.w, h = u.h;
-					if (w) this._unit._w = w;
-					if (h) this._unit._h = h;
-					var children = this._children;
-					for (var i = 0; i < children.length; i++) {
-						if (w) children[i].w = w;
-						if (h) children[i].h = h;
-					}
-				}
-			},
-		});
-
-		Object.defineProperties(this._unit, {
-			_w : { enumerable : false },
-			w : {
-				enumerable : true,
-				configurable : true,
-				get : function() { return this._w },
-				set : function(w) {
-					this._w = w;
-					var children = grid._children;
-					for (var i = 0; i < children.length; i++) {
-						children[i].w = w;
-					}
-				}
-			},
-			_h : { enumerable : false },
-			h : {
-				enumerable : true,
-				configurable : true,
-				get : function() { return this._h },
-				set : function(h) {
-					this._h = h;
-					var children = grid._children;
-					for (var i = 0; i < children.length; i++) {
-						children[i].h = h;
-					}
-				}
-			}
-		});
-
-		this._tiles = {};
+		P(this)
+			('hidden sealed').def('_coords');
 	},
-	tileAt : function(row, col) {
-		var coords = '@(' + row + ':' + col + ')';
-		var tile = Crafty(this._tileCID + ' ' + coords).get(); // TODO: manage multiple returns
-		if (!tile.length) {
-			tile = Crafty.e(this._tileCID, coords, '!')//.at(row, col);
+	coords : function(row, col) {
+		if (typeof row === 'undefined' && typeof col === 'undefined') {
+			return this._coords;
 		}
-		return tile;
-	}
-});
-
-var re_bang = /@\(([^\)]*)\)/;
-var re_args = /:/g;
-
-Crafty.c('!', {
-	init : function() {
-		var m;
-		for (var c in this.__c) {
-			if (m = re_bang.exec(c)) {
-				var args = m[1].split(re_args);
-				this['@!'].apply(this, args);
-			}
+		this._coords = { row : row, col : col };
+		if (this._parent) {
+			this.y = this._parent._y;
+			this.x = this._parent._x;
 		}
-		this.removeComponent('!');
-	}
-});
-
-
-Crafty.c('Tile', {
-	_row : 0,
-	_col : 0,
-	'@!' : function(r, c) {
-		this.y = this._parent._y + (this._row = +r) * this._h;
-		this.x = this._parent._x + (this._col = +c) * this._w;
-	},
-	init : function() {
-		this.requires('2D')
-
-		var W = Object.getOwnPropertyDescriptor(this, 'w');
-		var setw = W.set;
-		W.set = function(w) {
-			setw.call(this, w);
-			this.x = this._parent._x + this._col * w;
-		}
-		var H = Object.getOwnPropertyDescriptor(this, 'h');
-		var seth = H.set;
-		H.set = function(h) {
-			seth.call(this, h);
-			this.y = this._parent._y + this._row * h;
-		}
-		Object.defineProperties(this, {
-			_row : {},
-			row : {
-				configurable : true,
-				enumerable : true,
-				get : function() { return this._row },
-				set : function(r) {
-					this.removeComponent('@('+this._row+':'+this._col+')')
-						.addComponent('@('+r+':'+this._col+')');
-
-					this._row = r;
-					this.y = this._parent._y + r * this._h;
-				}
-			},
-			_col : {},
-			col : {
-				configurable : true,
-				enumerable : true,
-				get : function() { return this._col },
-				set : function(c) {
-					this.removeComponent('@('+this._row+':'+this._col+')')
-						.addComponent('@('+this._row+':'+c+')');
-
-					this._col = c;
-					this.x = this._parent._x + c * this._w;
-				}
-			},
-			w : W,
-			h : H
-		});
-	},
-	place : function(grid, row, col) {
-		grid.attach(this);
-		if (row) this.row = row;
-		if (col) this.col = col;
+		this.y += row * Map.unit.h;
+		this.x += col * Map.unit.w;
 		return this;
 	},
-	at : function(row, col) {
-		if (row !== undefined && col !== undefined) {
-			this.row = row;
-			this.col = col;
-			return this;
+	size : function(rows, cols) {
+		if (typeof rows === 'undefined' && typeof cols === 'undefined') {
+			return { rows : ~~(this._h / Map.unit.h), cols : ~~(this._w / Map.unit.w) };
 		}
-		return { row : this.row, col : this.col }
+		this.h = rows * Map.unit.h;
+		this.w = cols * Map.unit.w;
 	}
 });
-
-function Map() {}
-Map.prototype.display = function(grid) {
-	var tiles = this.tiles;
-	var rows = this.rows;
-	var cols = this.cols;
-	for (var y = 0; y < rows; y++) {
-		var row = tiles[y];
-		for (var x = 0; x < cols; x++) {
-			var tile = this.palette[row[x]];
-			grid.tileAt(y, x).requires('Canvas,Color').color(tile.color || 'rgba(0,0,0,0)')
-		}
-	}
-}
 
 Tiles = {
 	_maps : {},
 	_palettes : {},
 	load : function(url, success, error) {
+		if (typeof success !== 'function') {
+			success = function() {};
+		}
+		if (typeof error !== 'function') {
+			error = function() {};
+		}
+
 		var map;
 		if (url in Tiles._maps) {
 			map = Tiles._maps[url];
-			if (typeof success === 'function') {
-				success(map);
-			}
+			success(map);
 		} else {
-			map = Tiles._maps[url] = new Map;
-			var ajax = Ajax.get(url).success(function(data) {
-				var name = map.name = (data.name || url).trim();
+			Ajax.get(url).success(function(mapData) {
 
-				var tiles = map.tiles = data.tiles;
-
-				var rows = map.rows = data.rows || data.tiles.length;
-				var cols = data.cols;
-				if (typeof cols !== 'number') {
-					cols = 0;
-					// initialize `cols` to largest row
-					for (var i = 0; i < rows; i++) {
-						cols = Math.max(cols, tiles[i].length);
-					}
-				}
-				map.cols = cols;
-
-				var palette = data.palette;
+				var palette = mapData.palette;
 				if (typeof palette === 'string') {
+					var paletteUrl = palette;
 					if (paletteUrl in Tiles._palettes) {
-						map.palette = Tiles._palettes[paletteUrl];
-						if (typeof success === 'function') {
-							success(map);
-						}
+						mapiData.palette = Tiles._palettes[paletteUrl];
+						map = Tiles._maps[url] = new Map(mapData);
+						success(map);
 					} else {
-						Ajax.get(paletteUrl).success(function(data) {
-							map.palette = data;
-							if (typeof success === 'function') {
-								success(map);
-							}
+						Ajax.get(paletteUrl).success(function(paletteData) {
+							mapData.palette = Tiles._palettes[paletteUrl] = new Palette(paletteData);
+							map = Tiles._maps[url] = new Map(mapData);
+							success(map);
 						})
+						.error(error);
 					}
 				} else {
-					map.palette = palette;
-					if (typeof success === 'function') {
-						success(map);
-					}
+					mapData.palette = new Palette(palette);
+					map = Tiles._maps[url] = new Map(mapData);
+					success(map);
 				}
-			});
-			if (typeof error === 'function') {
-				ajax.error(error);
-			}
+			})
+			.error(error);
 		}
-		return map;
 	}
 }
 
-/*
-function tileMapDemo() {
-	Crafty.init();
-	Tiles.load('map.json');
-	var keydowns = 0, frames;
-	Crafty.bind('KeyDown', function(e) {
-		var ks = keydowns++;
-		if (ks === 0) frames = 0;
-	})
-	Crafty.bind('KeyUp', function(e) {
-		var ks = --keydowns;
-		if (ks === 0) frames = 0;
-	})
-	Crafty.bind('EnterFrame', function(e) {
-		var f = 0, x, y;
-		if (Crafty.keydown[k = Crafty.keys.UP_ARROW]) {
-			f++;
-			if (frames % 2 == 0) Crafty.viewport.y = Math.min(0, Crafty.viewport._y + tile.h / 2);
-		}
-		if (Crafty.keydown[k = Crafty.keys.RIGHT_ARROW]) {
-			f++;
-			if (frames % 2 == 0) Crafty.viewport.x = Math.max(-Tiles.current.size.cols * tile.w + Crafty.viewport._width, Crafty.viewport._x - tile.w / 2);
-		}
-		if (Crafty.keydown[k = Crafty.keys.DOWN_ARROW]) {
-			f++;
-			if (frames % 2 == 0) Crafty.viewport.y = Math.max(-Tiles.current.size.rows * tile.h + Crafty.viewport._height, Crafty.viewport._y - tile.h / 2);
-		}
-		if (Crafty.keydown[k = Crafty.keys.LEFT_ARROW]) {
-			f++;
-			if (frames % 2 == 0) Crafty.viewport.x = Math.min(0, Crafty.viewport._x + tile.w / 2);
-		}
-
-		if (f) frames++;
-	});
-	Crafty.bind('StartDrag', function(e) {
-		console.log(e);
-	});
-}
-*/
-
 function tiledDemo() {
 
-	Crafty.init(600, 400);
+	Crafty.init();
 	Crafty.viewport.mouselook(true);
 
-	var grid = Crafty.e('Grid');
 	Tiles.load('map.json', function(map) {
 		console.log('loading...');
-		map.display(grid);
+		map.show();
 		console.log('done.');
 	})
 }
--- src/util.js
+++ src/util.js
@@ -1,3 +1,141 @@
+var funargs = /^\s*function(?:\s+\w+)?\s*\(([^\)]*)\)/;
+function P(obj) {
+	var opt = { enumerable : true, configurable : true, writable : true };
+
+	function cmd(mods) {
+		if (arguments.length > 1) {
+			mods = Array.prototype.slice.call(arguments)
+		} else if (typeof mods === 'string') {
+			mods = mods.split(/[, ]+/g)
+		} else if (!(mods instanceof Array)) {
+			var ms = []
+			for (var m in mods) {
+				if (mods.hasOwnProperty(m) && mods[m]) {
+					ms.push(m);
+				}
+			}
+			mods = ms;
+		}
+		for (var m of mods) {
+			if (m === 'hidden') {
+				opt.enumerable = false;
+			} else if (m === 'sealed') {
+				opt.configurable = false;
+			}
+		}
+		return cmd
+	}
+	cmd.def = function(prop, o) {
+		var pdesc = Object.getOwnPropertyDescriptor(obj, prop)
+		if (pdesc && !pdesc.configurable) {
+			return;
+		}
+		switch (typeof o) {
+		case 'object':
+			for (var k in o) {
+				if (o.hasOwnProperty(k)) {
+					opt[k] = o[k];
+				}
+			}
+			break;
+		case 'function':
+			opt.get = o;
+			delete opt.writeable;
+			break;
+		default:
+			opt.value = o;
+		}
+		Object.defineProperty(obj, prop, opt);
+		opt = { enumerable : true, configurable : true, writable : true };
+		return cmd;
+	};
+	cmd.getset = function(prop, hprop, get, set) {
+		var pdesc = Object.getOwnPropertyDescriptor(obj, prop)
+		if (pdesc && !pdesc.configurable) {
+			return;
+		}
+		switch (arguments.length) {
+		case 1:
+			hprop = '_' + prop;
+			break;
+		case 2:
+			switch (typeof hprop) {
+			case 'string':
+				break;
+			case 'function':
+				var m = funargs.exec(hprop.toString());
+				if (m && m[1].trim().length) {
+					// function is a setter
+					set = hprop;
+				} else {
+					// function is a getter
+					get = hprop;
+				}
+				hprop = '_' + prop;
+				break;
+			case 'object':
+				var gs = hprop;
+				hprop = '_' + prop;
+				get = gs.get;
+				set = gs.set;
+			}
+			break;
+		case 3:
+			switch (typeof hprop) {
+			case 'boolean':
+				if (hprop) hprop = '_' + prop;
+			case 'string':
+				switch (typeof get) {
+				case 'function':
+					var m = funargs.exec(get.toString());
+					if (m && m[1].trim().length) {
+						// function is a setter
+						set = get;
+						get = void undefined;
+					}
+					break;
+				case 'object':
+					var gs = get;
+					get = gs.get;
+					set = gs.set;
+				}
+				break;
+			case 'function':
+				if (typeof get === 'function') {
+					set = get;
+					get = hprop;
+					hprop = '_' + prop;
+				}
+			}
+			break;
+		case 4:
+			if (hprop === true) {
+				hprop = '_' + prop;
+			}
+		}
+		if (hprop !== false) P(obj)('hidden').def(hprop);
+		delete opt.writable;
+		opt.get = get || (hprop !== false ? new Function('return this.' + hprop) : void undefined);
+		opt.set = set || (hprop !== false ? new Function('v', 'this.' + hprop + '=v') : void undefined);
+		Object.defineProperty(obj, prop, opt);
+		opt = { enumerable : true, configurable : true, writable : true };
+		return cmd;
+	};
+	cmd.constant = function(prop, val) {
+		var pdesc = Object.getOwnPropertyDescriptor(obj, prop)
+		if (pdesc && !pdesc.configurable) {
+			return;
+		}
+		if (typeof val !== 'undefined') {
+			opt.value = val;
+		}
+		opt.writable = false;
+		Object.defineProperty(obj, prop, opt);
+		opt = { enumerable : true, configurable : true, writable : true };
+		return cmd;
+	}
+	return cmd
+}
 Properties = {
 	private : function(obj, key) {
 		Object.defineProperty(obj, key, { enumerable : false })
